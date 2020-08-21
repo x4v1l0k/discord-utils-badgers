7d44a61af30aa17c9396ef70360bcbc7d1a098fc
root <root@SupTecnico.IDC.local>
2020-08-21 08:19:53 +0200

    test

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
+client.on('ready', () => {
+	console.log("Connected as " + client.user.tag)
+	var generalChannel = client.channels.get("725983086428553318") // Replace with known channel ID
+	generalChannel.send("Hello, server, i'm a new JS bot! But, i'm under construction...")
+})
+
+// Get your bot's secret token from:
+// https://discordapp.com/developers/applications/
+// Click on your application -> Bot -> Token -> "Click to Reveal Token"
+bot_secret_token = "NjEyNTc5MzAxMzA0NDM0Njkx.XvWY4Q.xUV1YsQdoPmzCL4g0MucOjwgoOw"
+
+client.login(bot_secret_token)
